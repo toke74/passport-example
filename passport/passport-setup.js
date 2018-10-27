1c9f514fdc5dc59c891d8d61ee9982bf0c09e5e7
mele <mele@example.com>
2018-10-26 22:07:24 -0400

    securing OAuth client IDs and clent Secrete

--- passport/passport-setup.js
+++ passport/passport-setup.js
@@ -1,12 +1,13 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
 
 //creating new google strategy instance
 passport.use(
   new GoogleStrategy({
     // options for google strategy
-    clientID: "",
-    clientSecret: "",
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret,
     callbackURI: ""
   }),
   () => {
